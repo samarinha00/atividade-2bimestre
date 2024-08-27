5052696c3018af00b899bed69fa73dafa0b00ac2
samarinha00 <salapjdm224@gmail.com>
2024-08-27 11:08:04 -0300

    player pulo

--- Meu Game 3D/Assets/scripts/Player.cs
+++ Meu Game 3D/Assets/scripts/Player.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
     public int velocidade = 5;
     public Rigidbody rb;
+
+    public int forcaPulo = 10;
+
+    public bool noChao = false;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("eae man");
         TryGetComponent(out rb);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Chao")
+        {
+            noChao = true;
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -22,6 +38,15 @@ public class Player : MonoBehaviour
         float y = Input.GetAxis("Vertical");
 
         UnityEngine.Vector3 direcao = new UnityEngine.Vector3(x, 0, y);
-        rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        rb.AddForce(direcao * (velocidade * Time.deltaTime), ForceMode.Impulse);
+        if(Input.GetKeyDown(KeyCode.Space) && noChao == true) //PULO
+        {
+                rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+                noChao = false;
+        }
+        if (transform.position.y < -5)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
